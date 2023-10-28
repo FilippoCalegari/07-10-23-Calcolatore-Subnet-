94e28e6187f2b61ac1c04dc159ace09882db7221
FilippoCalegari <filippo.calegari06@gmail.com>
2023-10-28 11:51:50 +0200

    Completamento Classe C e inizio Classe B.

--- 07-10-23 (Calcolatore Subnet)/Form1.cs
+++ 07-10-23 (Calcolatore Subnet)/Form1.cs
@@ -15,20 +15,46 @@ namespace _07_10_23__Calcolatore_Subnet_
         public int esponente;
         public void CalcoloSubnet()
         {
-            int subnet = 0;
+            double subnet = 0;
+            double subnet2 = 0;
 
             if (txtb_Class.Text == "Classe C")
             {
-                for (int i = 7; i >= esponente ; i--)
+                for (double i = 7; i >= 8 - esponente; i--)
                 {
-                    //subnet += int.Parse(Math.Pow(2, i));
+                    subnet += Math.Pow(2, i);
                 }
 
-                txtb_Subnet.Text = "255.255.255." + (128 + 
+                txtb_Subnet.Text = "255.255.255." + (subnet);
             }
             else if (txtb_Class.Text == "Classe B")
             {
+                double bitAvanzati = esponente - 8;
 
+                if (esponente > 8)
+                {
+                    for (double i = 7; i >= 8 - (esponente - bitAvanzati); i--)
+                    {
+                        subnet += Math.Pow(2, i);
+                    }
+
+                    for (double i = 7; i >= bitAvanzati; i--)
+                    {
+                        subnet2 += Math.Pow(2, i);
+                    }
+
+                    txtb_Subnet.Text = "255.255." + (subnet) + (subnet2);
+                }
+                else
+                {
+                    for (double i = 7; i >= 8 - esponente; i--)
+                    {
+                        subnet += Math.Pow(2, i);
+                    }
+
+                    txtb_Subnet.Text = "255.255." + (subnet) + ".0";
+                }
+                
             }
             else if (txtb_Class.Text == "Classe A")
             {
@@ -62,22 +88,25 @@ namespace _07_10_23__Calcolatore_Subnet_
 
         private void btn_Calcolare_Click(object sender, EventArgs e)
         {
-            esponente = (int)(Math.Log(int.Parse(txtb_NumHost.Text) + 2, 2) + Math.Log(int.Parse(txtb_NumSottoreti.Text) + 2, 2));
+            esponente = (int)(Math.Log(int.Parse(txtb_NumSottoreti.Text) + 2, 2));
 
             if (Math.Log(int.Parse(txtb_NumHost.Text) + 2, 2) + Math.Log(int.Parse(txtb_NumSottoreti.Text) + 2, 2) <= 8)
             {
                 txtb_Class.Text = "Classe C";
                 txtb_IndirizzoBase.Text = "192.168.0.0";
+                CalcoloSubnet();
             }
             else if (Math.Log(int.Parse(txtb_NumHost.Text) + 2, 2) + Math.Log(int.Parse(txtb_NumSottoreti.Text) + 2, 2) <= 16)
             {
                 txtb_Class.Text = "Classe B";
                 txtb_IndirizzoBase.Text = "172.16.0.0";
+                CalcoloSubnet();
             }
             else if (Math.Log(int.Parse(txtb_NumHost.Text) + 2, 2) + Math.Log(int.Parse(txtb_NumSottoreti.Text) + 2, 2) <= 24)
             {
                 txtb_Class.Text = "Classe A";
                 txtb_IndirizzoBase.Text = "10.0.0.0";
+                CalcoloSubnet();
             }
             else
             {
